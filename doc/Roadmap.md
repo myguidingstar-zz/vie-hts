fc26231e3f6d20f9662884d9160f94622383825d
Dương Yang ヤン Nguyễn <cmpitg@gmail.com>
2012-02-28 18:25:04 +0700

    Mostly clean up, add TODO

---
+++ doc/Roadmap.md
@@ -0,0 +1,39 @@
+# Kế hoạch triển khai dự án
+
+## Giai đoạn một
+
+Xây dựng giọng nói đầu tiên.
+
+### Mục tiêu:
+
+* Tạo ra giọng đọc đầu tiên.
+* Xây dựng một chương trình đọc tiếng Việt đơn giản viết bằng Clojure.
+* Tạo giọng đọc tương ứng cho Espeak.
+
+### Cách thức tổ chức thực hiện:
+
+* Giọng được ghi âm là giọng một thành viên trong nhóm.
+* Nhóm gặp mặt làm việc trực tiếp do đặc thù sản phẩm là âm thanh.
+* Thường xuyên cập nhật thông tin và sản phẩm trên Github.
+* Tiếp thu ý kiến đóng góp của cộng đồng.
+
+### Thời gian:
+
+* Dự kiến kéo dài khoảng 1.5 tháng để hoàn thành tất cả các mục tiêu
+  với chất lượng tốt
+* Tối đa không quá 3 tháng nếu có sự cố
+
+## Giai đoạn hai:
+
+* Bổ sung một số giọng đọc
+* Công bố rộng rãi kết quả của giai đoạn một
+* Công bố hướng dẫn chi tiết cho việc tạo các giọng nói tiếp theo.
+* Nhóm A11y-vietnamese hỗ trợ các nhóm khác trong việc thực hiện và
+  công bố kết quả.
+
+## Giai đoạn ba
+
+Triển khai ứng dụng rộng rãi
+
+* Nghiên cứu mở rộng khả năng ứng dụng.
+* Phát triển bổ sung các tính năng nâng cao.
--- src/a11y/core.clj
+++ src/a11y/core.clj
@@ -1,128 +1,150 @@
-; * Tên tiếng Anh dùng trong chương trình:
-	; * Vần: rhyme
-	; * Phụ âm đầu: consonant
-	; * Âm chính: major-vowel
-	; * Đầu vần: vowel-prefix
-	; * Đuôi vần: vowel-suffix
+;;; Tên tiếng Anh dùng trong chương trình:
+;;;
+;;; * Vần: rhyme
+;;; * Phụ âm đầu: consonant
+;;; * Âm chính: major-vowel
+;;; * Đầu vần: vowel-prefix
+;;; * Đuôi vần: vowel-suffix
+
 (ns a11y.core)
+
+;;; All consonants
 (def consonants '[b c ch d đ f g h j k kh l m n ng nh p r s t th tr v w x z])
+
+;;; All vowels
 (def major-vowels '[a ă â e ê i o ơ ô u ư y])
+
 (def vowel-suffixes '[nil c ch i m n ng nh o p t u y])
-(def diacritics-available '{nil 6 c 2 ch 2 i 6 m 6 n 6 ng 6 nh 6 o 6 p 2 t 2 u 6 y 6})
+
+(def diacritics-available
+  '{nil 6 c 2 ch 2 i 6 m 6 n 6 ng 6 nh 6 o 6 p 2 t 2 u 6 y 6})
+
 (def vowel-prefixes '[nil i o u ư])
+
 (def diacritics-6 '[nil ̀ ́ ̉ ̃ ̣])
+
 (def diacritics-2 '[́ ̣])
+
 (def allowed-suffixes
-'{	 "a" [nil c ch i m n ng nh o p t u y]
-	"ia" [nil                           ]
-	"oa" [nil c ch i m n ng nh o p t   y]
-	"ua" [nil                           ]
-	"ưa" [nil                           ]
+  '{ "a"   [nil c ch i m n ng nh o p t u y]
+     "ia"  [nil                           ]
+     "oa"  [nil c ch i m n ng nh o p t   y]
+     "ua"  [nil                           ]
+     "ưa"  [nil                           ]
 
-	 "ă" [nil c      m n ng      p t    ]
-	"iă" []
-	"oă" [    c      m n ng      p t    ]
-	"uă" [] ;biểu diễn qua oă
+     "ă"   [nil c      m n ng      p t    ]
+     "iă"  []
+     "oă"  [    c      m n ng      p t    ]
+     "uă"  []                           ; biểu diễn qua oă
 
-	 "â" [nil c      m n ng      p t u y]
-	"iâ" [] ;dùng y như một phụ âm
-	"oâ" []
-	"uâ" [    c      m n ng      p t    ]
-	"ưâ" []
+     "â"   [nil c      m n ng      p t u y]
+     "iâ"  []                           ; dùng y như một phụ âm
+     "oâ"  []
+     "uâ"  [    c      m n ng      p t    ]
+     "ưâ"  []
 
-	 "e" [nil c      m n ng      p t    ]
-	"ie" []
-	"oe" [nil c      m n ng    o p t    ]
-	"ue" [] ;biểu diễn qua oe
-	"ưe" []
+     "e"   [nil c      m n ng      p t    ]
+     "ie"  []
+     "oe"  [nil c      m n ng    o p t    ]
+     "ue"  []                           ; biểu diễn qua oe
+     "ưe"  []
 
-	 "ê" [nil   ch   m n ng nh   p t    ]
-	"iê" [    c      m n ng      p t u  ]
-	"oê" [] ;biểu diễn qua uê
-	"uê" [nil   ch   m n ng nh   p t u y]
-	"ưê" []
+     "ê"   [nil   ch   m n ng nh   p t    ]
+     "iê"  [    c      m n ng      p t u  ]
+     "oê"  []                           ; biểu diễn qua uê
+     "uê"  [nil   ch   m n ng nh   p t u y]
+     "ưê"  []
 
-	 "i" [nil   ch   m n ng nh   p t u  ]
-	"ii" []
-	"oi" [] ;đây là o- /i, đừng nhầm với /o -i
-	"ui" [] ;đây là u- /i, đừng nhầm với /u -i ; biểu diễn qua uy
-	"ưi" []
+     "i"   [nil   ch   m n ng nh   p t u  ]
+     "ii"  []
+     "oi"  []    ; đây là o- /i, đừng nhầm với /o -i
+     "ui"  []    ; đây là u- /i, đừng nhầm với /u -i ; biểu diễn qua
+                 ; uy
+     "ưi"  []
 
-	 "o" [nil c    i m n ng      p t    ]
-	"io" []
-	"oo" [    c          ng             ]
-	"uo" []
-	"ưo" []
+     "o"   [nil c    i m n ng      p t    ]
+     "io"  []
+     "oo"  [    c          ng             ]
+     "uo"  []
+     "ưo"  []
 
-	 "ô" [nil c    i m n ng nh   p t    ]
-	"iô" []
-	"oô" []
-	"uô" [    c    i m n ng      p t    ]
-	"ưô" []
+     "ô"   [nil c    i m n ng nh   p t    ]
+     "iô"  []
+     "oô"  []
+     "uô"  [    c    i m n ng      p t    ]
+     "ưô"  []
 
-	 "ơ" [nil c    i m n ng      p t    ]
-	"iơ" []
-	"oơ" []
-	"uơ" [nil      i                 u  ]
-	"ươ" [    c    i m n ng      p t u  ]
+     "ơ"   [nil c    i m n ng      p t    ]
+     "iơ"  []
+     "oơ"  []
+     "uơ"  [nil      i                 u  ]
+     "ươ"  [    c    i m n ng      p t u  ]
 
-	 "u" [nil c    i m n ng      p t   y]
-	"iu" [] ;đây là i- /u, đừng nhầm với /i -u
-	"ou" []
-	"uu" []
-	"ưu" [] ;đây là ư- /u, đừng nhầm với /ư -u
+     "u"   [nil c    i m n ng      p t   y]
+     "iu"  []                       ; đây là i- /u, đừng nhầm với /i -u
+     "ou"  []
+     "uu"  []
+     "ưu"  []                       ; đây là ư- /u, đừng nhầm với /ư -u
 
-	 "ư" [nil c    i m n ng nh   p t u  ]
-	"iư" []
-	"oư" []
-	"uư" []
-	"ưư" []
+     "ư"   [nil c    i m n ng nh   p t u  ]
+     "iư"  []
+     "oư"  []
+     "uư"  []
+     "ưư"  []
 
-	 "y" [                              ] ;dùng i thay thế
-	"iy" []
-	"oy" []
-	"uy" [nil c ch   m n ng nh   p t u  ]
-	"ưy" []})
-;(def allowed-suffixes-strict) -->binding
+     "y"   [                              ] ; dùng i thay thế
+     "iy"  []
+     "oy"  []
+     "uy"  [nil c ch   m n ng nh   p t u  ]
+     "ưy"  []})
+
+;; (def allowed-suffixes-strict) --> binding?
+
 (defn count-allowed-suffixes [major-vowel-set]
-	(reduce + 
-		(map #(get diacritics-available %1)
-			(get allowed-suffixes major-vowel-set))))
+  (reduce + (map #(get diacritics-available %)
+                 (get allowed-suffixes major-vowel-set))))
+
 (defn get-major-vowel-set [major-vowel]
-	(map #(str %1 major-vowel)
-		vowel-prefixes))
+  (map #(str %1 major-vowel)
+       vowel-prefixes))
+
 (defn count-rhymes-by-single-major-vowel [major-vowel]
-	(reduce + 
-		(map count-allowed-suffixes
-			(get-major-vowel-set major-vowel))))
+  (reduce + (map count-allowed-suffixes
+                 (get-major-vowel-set major-vowel))))
+
 (defn count-rhymes []
-	(reduce +
-		(map count-rhymes-by-single-major-vowel
-			major-vowels)))
+  (reduce + (map count-rhymes-by-single-major-vowel
+                 major-vowels)))
+
 (defn suffix-to-diacritics [vowel-suffix]
-	(cond
-		(= (get diacritics-available vowel-suffix) 6) diacritics-6
-		(= (get diacritics-available vowel-suffix) 2) diacritics-2
-		))
+  (cond
+   (= (get diacritics-available vowel-suffix) 6) diacritics-6
+   (= (get diacritics-available vowel-suffix) 2) diacritics-2
+   ))
+
 (defn add-diacritics-to-rhyme-set [[major-vowel-set vowel-suffix]]
-	(reduce conj []
-		(map #(str major-vowel-set %1 vowel-suffix)
-			(suffix-to-diacritics vowel-suffix))))
+  (reduce conj []
+          (map #(str major-vowel-set %1 vowel-suffix)
+               (suffix-to-diacritics vowel-suffix))))
+
 (defn make-rhyme-sets [major-vowel-set]
-	(reduce conj []
-		(map #(conj [major-vowel-set] %1)
-			(get allowed-suffixes major-vowel-set))))
+  (reduce conj []
+          (map #(conj [major-vowel-set] %1)
+               (get allowed-suffixes major-vowel-set))))
+
 (defn make-rhymes-by-single-major-vowel [major-vowel]
-	(apply concat
-		(map add-diacritics-to-rhyme-set
-			(apply concat
-				(map make-rhyme-sets (get-major-vowel-set major-vowel))))))
+  (apply concat
+         (map add-diacritics-to-rhyme-set
+              (apply concat
+                     (map make-rhyme-sets (get-major-vowel-set major-vowel))))))
+
 (defn make-all-rhymes []
-	(apply concat
-		(map make-rhymes-by-single-major-vowel major-vowels)
-	))
-;(apply println (make-all-rhymes))
-(binding [*out* (java.io.FileWriter. "rhyme-list.txt")] ;overwrite mode
-	(dorun
-		(for [ rhyme (make-all-rhymes)]
-			(println rhyme))))
+  (apply concat
+         (map make-rhymes-by-single-major-vowel major-vowels)))
+
+;; (apply println (make-all-rhymes))
+
+(binding [*out* (java.io.FileWriter. "rhyme-list.txt")] ; overwrite mode
+  (dorun
+   (for [ rhyme (make-all-rhymes)]
+     (println rhyme))))
