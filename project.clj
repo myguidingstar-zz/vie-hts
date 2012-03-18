e3de65dc3a82add13e7e73d527c97354a4eead5c
Hoàng Minh Thắng <hoangminhthang@ktqd.org>
2012-03-18 12:22:56 +0700

    Samples

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject a11y "1.0.0-SNAPSHOT"
   :description "Producing list of rhymes for vie-hts@a11y-vietnamese project"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+    [lein-marginalia "0.6.0"]])
